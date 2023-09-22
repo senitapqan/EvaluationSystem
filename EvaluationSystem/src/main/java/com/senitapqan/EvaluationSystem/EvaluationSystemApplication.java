4dd60cc73e44c1c0cc02fed38cc404644570e13c
senitapqan <maskeugalievd@gmail.com>
2023-09-22 21:53:34 +0600

    First try

---
+++ EvaluationSystem/src/main/java/com/senitapqan/EvaluationSystem/EvaluationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.senitapqan.EvaluationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationSystemApplication.class, args);
+	}
+
+}
---
+++ EvaluationSystem/src/test/java/com/senitapqan/EvaluationSystem/EvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senitapqan.EvaluationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
